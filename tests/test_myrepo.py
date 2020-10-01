2ff8042c7eb8210d7b8b6c1d0f8eb9910a39eac8
JLybianto <jesse.lybianto@gmail.com>
2020-10-01 02:44:35 +0000

    Add main.py and update test script

--- tests/test_myrepo.py
+++ tests/test_myrepo.py
@@ -1,5 +1,13 @@
-from myrepolib import repomod
+from myrepolib import *
 
 def test_func():
 	result = repomod.myfunc()
 	assert result == 1
+
+def test_index():
+    main.app.testing = True
+    client = main.app.test_client()
+
+    r = client.get("/")
+    assert r.status_code == 200
+    assert "Hello World!" in r.data.decode("utf-8")
