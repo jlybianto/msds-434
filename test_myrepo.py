6afb8531fd51038c84f26024877523be3669e710
JLybianto <jesse.lybianto@gmail.com>
2020-10-01 16:26:45 +0000

    Change script location to main folder

--- test_myrepo.py
+++ test_myrepo.py
@@ -1,14 +1,10 @@
-from myrepolib import repomod
-
 def test_func():
 	result = repomod.myfunc()
 	assert result == 1
 
-from myrepolib import main
-
 def test_index():
-    main.app.testing = True
-    client = main.app.test_client()
+    app.testing = True
+    client = app.test_client()
 
     r = client.get("/")
     assert r.status_code == 200
