7f26cb4526de523da39e2ff6eca541265e14725c
JLybianto <jesse.lybianto@gmail.com>
2020-10-02 08:45:39 +0000

    Add JSONify

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask import jsonify
 
 app = Flask(__name__)
 
@@ -7,5 +8,12 @@ def hello():
 	"""Return a friendly HTTP greeting."""
 	return "Hello World!"
 
+@app.route('/name/<value>')
+def name(value):
+    val = {
+        "value": value
+    }
+    return jsonify(val)
+    
 if __name__ == "__main__":
 	app.run(host="127.0.0.1", port=8080, debug=True)
