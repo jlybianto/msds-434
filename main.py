1f13830d2136c952ca6fc71b18a4171bb2d36dac
JLybianto <jesse.lybianto@gmail.com>
2020-10-02 08:49:22 +0000

    JSONify added

--- main.py
+++ main.py
@@ -7,5 +7,12 @@ def hello():
 	"""Return a friendly HTTP greeting."""
 	return "Hello World!"
 
+@app.route('/name/<value>')
+def name(value):
+    val = {
+        "value": value
+    }
+    return jsonify(val)
+    
 if __name__ == "__main__":
 	app.run(host="127.0.0.1", port=8080, debug=True)
