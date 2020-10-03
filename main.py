566f40b411d9106d4ac00e41b4cc0af6579e5eeb
JLybianto <jesse.lybianto@gmail.com>
2020-10-03 01:33:48 +0000

    Change main.py to test build trigger

--- main.py
+++ main.py
@@ -14,6 +14,13 @@ def name(value):
         "value": value
     }
     return jsonify(val)
+
+@app.route('/creator/')
+def creator():
+    val = {
+        "value": "JLybianto"
+    }
+    return jsonify(val)
     
 if __name__ == "__main__":
 	app.run(host="127.0.0.1", port=8080, debug=True)
