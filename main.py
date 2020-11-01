e954afb74ac2376c0b1912729f7fdda54dd5f60a
JLybianto <jesse.lybianto@gmail.com>
2020-11-01 20:40:20 +0000

    Initialize development environment

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
 	"""Return a friendly HTTP greeting."""
-	return "Hello World!"
+	return "Development Environment!"
 
 @app.route('/name/<value>')
 def name(value):
